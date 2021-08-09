e61158a2c8bffc18388371c2f0474e5367adab85
naveen <016naveen.kumar@gmail.com>
2021-08-09 13:21:50 +0530

    MemeDroid

--- app/src/main/java/com/example/memedroid/MainActivity.kt
+++ app/src/main/java/com/example/memedroid/MainActivity.kt
@@ -26,6 +26,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         memeImage=findViewById(R.id.memeImage)
         progressBar=findViewById(R.id.progressBar)
     }
